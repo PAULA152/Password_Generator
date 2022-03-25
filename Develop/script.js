58fd99a64f151d8132496b8b930645286130a0cb
PAULA152 <atienzopa152@gmail.com>
2022-03-25 14:44:35 -0700

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,91 +1,9 @@
 // // Assignment Code
 let lengthOfPassword = 8;
-let lettersLower = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-let lettersUpper = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-let theNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let specialChar = [
-  "!",
-  "#",
-  "$",
-  "%",
-  "&",
-  "(",
-  ")",
-  "*",
-  "-",
-  ".",
-  "/",
-  ";",
-  ":",
-  "<",
-  "=",
-  ">",
-  "@",
-  "]",
-  "[",
-  "^",
-  "_",
-  "`",
-  "{",
-  "|",
-  "}",
-  "~",
-  '"',
-];
+let lettersLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+let lettersUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+let theNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
+let specialChar = ['!', '#', '$', '%', '&', '(', ')', '*', '-', '.', '/', ';', ':', '<', '=', '>', '@', ']', '[', '^', '_', '`', '{', '|', '}', '~', '"',];
 let userChoice = [];
 let generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
@@ -96,7 +14,7 @@ function generatePassword() {
     let passwordIndex = Math.floor(Math.random() * userChoice.length);
     password = password + userChoice[passwordIndex];
   }
-  console.log(password);
+  console.log(password)
   return password;
 }
 
@@ -113,15 +31,9 @@ function writePassword() {
 }
 
 function prompts() {
-  lengthOfPassword = parseInt(
-    prompt("How long would you like your password to be? (8 - 128)")
-  );
+  lengthOfPassword = parseInt(prompt("How long would you like your password to be? (8 - 128)"));
 
-  if (
-    isNaN(lengthOfPassword) ||
-    lengthOfPassword < 8 ||
-    lengthOfPassword > 128
-  ) {
+  if (isNaN(lengthOfPassword) || lengthOfPassword < 8 || lengthOfPassword > 128) {
     alert("the length has to be a number between 8 and 128");
     return false;
   }
